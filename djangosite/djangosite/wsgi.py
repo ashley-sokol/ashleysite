71f5ea6a079ec8229c78046079d203a4b2aa63c7
mqx4vk <143546242+mqx4vk@users.noreply.github.com>
2024-06-09 17:34:25 -0400

    wsgi.py: commented wsgi file for pythonanywhere

--- djangosite/djangosite/wsgi.py
+++ djangosite/djangosite/wsgi.py
@@ -14,3 +14,20 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangosite.settings')
 
 application = get_wsgi_application()
+
+# # 
+# import os
+# import sys
+
+# # Add your project directory to the sys.path
+# project_home = '/var/www/your_username_pythonanywhere_com'
+# if project_home not in sys.path:
+#     sys.path.append(project_home)
+
+# # Set environment variables
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangosite.settings')
+
+# # Start Django application
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
